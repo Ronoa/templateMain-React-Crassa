df7c3e7f69b6c5f9dea04c9f73d43e5e16fc82a3
JoseFlores <dens205@gmail.com>
2020-11-07 16:30:57 -0500

    [UPDATE] quitando consola

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,29 +4,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useComponentWillMount } from 'lib/hooks'
 
 import counterDucks from 'reducers/counter'
-import { Button, makeStyles, Paper, Typography } from '@material-ui/core'
+import { Button, Paper, Typography } from '@material-ui/core'
 
 const { addCount, removeCount, addCountFromServer } = counterDucks.creators
 
-const useStyles = makeStyles((theme) => ({
-  content: {
-    flexGrow  : 1,
-    padding   : theme.spacing(0,9.5),
-    backGround: 'red'
-  },
-  toolbarChildren: {
-    display       : 'flex',
-    alignItems    : 'center',
-    justifyContent: 'flex-end',
-    padding       : theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar
-  }
-}))
-
 export default () => {
-  const classes = useStyles()
-  console.log('JOSE: classes', classes)
   const dispatch = useDispatch()
   const count = useSelector(({ counter: { count } }) => count)
 
