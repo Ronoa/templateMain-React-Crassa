57ba6c09fc4eef4a154aa222a4f14f6c1231173b
ronoa <dens205@gmail.com>
2022-05-04 18:16:45 -0500

    [UPDATE]

--- src/containers/Header/listItems.js
+++ src/containers/Header/listItems.js
@@ -9,6 +9,7 @@ import PeopleIcon from '@mui/icons-material/People'
 import BarChartIcon from '@mui/icons-material/BarChart'
 import LayersIcon from '@mui/icons-material/Layers'
 import AssignmentIcon from '@mui/icons-material/Assignment'
+import CategoryIcon from '@mui/icons-material/Category'
 
 export const mainListItems = (
   <React.Fragment>
@@ -29,6 +30,12 @@ export const mainListItems = (
         <PeopleIcon />
       </ListItemIcon>
       <ListItemText primary='Customers' />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <CategoryIcon />
+      </ListItemIcon>
+      <ListItemText primary='Products' />
     </ListItemButton>
     <ListItemButton>
       <ListItemIcon>
